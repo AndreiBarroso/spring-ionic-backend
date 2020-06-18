82182ff7e407166b521d63cc370c7ad26d336d6e
AndreiBarroso <andrei.mesquita@globomail.com>
2020-06-18 20:16:36 -0300

    configuração do h2

--- src/main/java/com/andreibarroso/springionic/domain/Categoria.java
+++ src/main/java/com/andreibarroso/springionic/domain/Categoria.java
@@ -1,6 +1,5 @@
 package com.andreibarroso.springionic.domain;
 
-
 import lombok.Data;
 
 import javax.persistence.Entity;
@@ -11,7 +10,10 @@ import java.io.Serializable;
 
 
 @Data
+@Entity
 public class Categoria  implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
