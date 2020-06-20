1fae666ca284003ec83fef5e73cbf2713837126a
AndreiBarroso <andrei.mesquita@globomail.com>
2020-06-20 15:02:40 -0300

    implementação dos estados e cidades

--- src/main/java/com/andreibarroso/springionic/SpringIonicApplication.java
+++ src/main/java/com/andreibarroso/springionic/SpringIonicApplication.java
@@ -1,8 +1,12 @@
 package com.andreibarroso.springionic;
 
 import com.andreibarroso.springionic.domain.Categoria;
+import com.andreibarroso.springionic.domain.Cidade;
+import com.andreibarroso.springionic.domain.Estado;
 import com.andreibarroso.springionic.domain.Produto;
 import com.andreibarroso.springionic.repositories.CategoriaRepository;
+import com.andreibarroso.springionic.repositories.CidadeRepository;
+import com.andreibarroso.springionic.repositories.EstadoRepository;
 import com.andreibarroso.springionic.repositories.ProdutoRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,10 +20,14 @@ public class SpringIonicApplication implements CommandLineRunner {
 
 	private CategoriaRepository categoriaRepository;
 	private ProdutoRepository produtoRepository;
+	private EstadoRepository estadoRepository;
+	private CidadeRepository cidadeRepository;
 
-	public SpringIonicApplication (CategoriaRepository categoriaRepository, ProdutoRepository produtoRepository) {
+	public SpringIonicApplication (CategoriaRepository categoriaRepository, ProdutoRepository produtoRepository, EstadoRepository estadoRepository, CidadeRepository cidadeRepository) {
 		this.categoriaRepository = categoriaRepository;
 		this.produtoRepository = produtoRepository;
+		this.cidadeRepository = cidadeRepository;
+		this.estadoRepository = estadoRepository;
 	}
 
 	public static void main(String[] args) {
@@ -36,6 +44,14 @@ public class SpringIonicApplication implements CommandLineRunner {
 		Produto p2 = new Produto(null, "IMPRESSORA", 300.00);
 		Produto p3 = new Produto(null, "MOUSE", 1400.00);
 
+		Estado estado1 = new Estado(null, "Minas gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+
+		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
+		Cidade cidade2 = new Cidade(null, "Taubate", estado2);
+		Cidade cidade3 = new Cidade(null, "Campinas", estado1);
+
+
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
@@ -43,6 +59,14 @@ public class SpringIonicApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
+
+		estado1.getCidades().addAll(Arrays.asList(cidade1));
+		estado1.getCidades().addAll(Arrays.asList(cidade2));
+		estado2.getCidades().addAll(Arrays.asList(cidade2));
+
+
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
--- src/main/java/com/andreibarroso/springionic/controller/ProdutoResource.java
+++ src/main/java/com/andreibarroso/springionic/controller/ProdutoResource.java
@@ -1,4 +1,12 @@
 package com.andreibarroso.springionic.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/produtos")
 public class ProdutoResource {
+
+
 }
