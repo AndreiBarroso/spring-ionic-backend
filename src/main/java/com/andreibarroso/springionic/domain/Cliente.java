6d2df6e8ef05d03dc7c6dd8dcc68958ad72340ae
AndreiBarroso <andrei.mesquita@globomail.com>
2020-06-20 19:33:28 -0300

    implementation and fix

---
+++ src/main/java/com/andreibarroso/springionic/domain/Cliente.java
@@ -0,0 +1,55 @@
+package com.andreibarroso.springionic.domain;
+
+import com.andreibarroso.springionic.domain.enums.TipoCliente;
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+public class Cliente implements Serializable {
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+    private String nome;
+    @Email
+    private String email;
+    @CPF
+    private String cpfOuCnpj;
+    private Integer tipo;
+
+    private final List<Endereco> enderecos = new ArrayList<>();
+
+    private  final Set<String> telefones = new HashSet<>();
+
+    public Cliente () {
+
+    }
+
+    public Cliente(Integer id, String nome, @Email String email, @CPF String cpfOuCnpj, TipoCliente tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.tipo = tipo.getCod();
+    }
+
+    public TipoCliente getTipo ()  {
+        return TipoCliente.toEnum(tipo);
+    }
+   public void setTipo (TipoCliente tipo) {
+        this.tipo = tipo.getCod ();
+   }
+}
---
+++ src/main/java/com/andreibarroso/springionic/domain/Endereco.java
@@ -0,0 +1,44 @@
+package com.andreibarroso.springionic.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Data
+public class Endereco implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+
+    private Cidade cidade;
+    private Cliente cliente;
+
+    public Endereco () {
+
+    }
+
+    public Endereco(Cidade cidade,Integer id, String logradouro, String numero, String complemento, String bairro, String cep, Cliente cliente) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cliente = cliente;
+        this.cidade = cidade;
+    }
+}
+
+
