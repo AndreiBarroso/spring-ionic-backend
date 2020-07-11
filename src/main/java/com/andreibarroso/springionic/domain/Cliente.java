345ebb0634f28304292d8103c2b306b6a537d983
AndreiBarroso <andrei.mesquita@globomail.com>
2020-07-10 22:13:08 -0300

    implementação de roles

--- src/main/java/com/andreibarroso/springionic/domain/Cliente.java
+++ src/main/java/com/andreibarroso/springionic/domain/Cliente.java
@@ -1,5 +1,6 @@
 package com.andreibarroso.springionic.domain;
 
+import com.andreibarroso.springionic.domain.enums.Perfil;
 import com.andreibarroso.springionic.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Data
@@ -38,11 +40,17 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "TELEFONE")
     private  final Set<String> telefones = new HashSet<>();
 
+    @ElementCollection(fetch=FetchType.EAGER)
+    @CollectionTable(name = "PERFIS")
+    private final Set<Integer> perfis =  new HashSet<>();
+
     @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private final List<Pedido> pedidos = new ArrayList<>();
 
     public Cliente () {
+
+        addPerfil(Perfil.CLIENTE);
 
     }
 
@@ -52,8 +60,9 @@ public class Cliente implements Serializable {
         this.email = email;
         this.senha = senha;
         this.cpfOuCnpj = cpfOuCnpj;
-
         this.tipo = (tipo==null) ? null : tipo.getCod();
+
+        addPerfil(Perfil.CLIENTE);
     }
 
     public TipoCliente getTipo ()  {
@@ -62,4 +71,13 @@ public class Cliente implements Serializable {
    public void setTipo (TipoCliente tipo) {
         this.tipo = tipo.getCod ();
    }
+    public Set<Perfil> getPerfis(){
+        return  perfis.stream().map(Perfil::toEnum).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil){
+        perfis.add(perfil.getCod());
+    }
+
+
 }
