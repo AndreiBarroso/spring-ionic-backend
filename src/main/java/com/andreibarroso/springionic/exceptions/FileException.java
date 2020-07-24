62fd1cddac5caac6e0a3176e916aed68d2aa7cfd
AndreiBarroso <andrei.mesquita@globomail.com>
2020-07-24 15:05:36 -0300

    Tratando exceções adequadamente

---
+++ src/main/java/com/andreibarroso/springionic/exceptions/FileException.java
@@ -0,0 +1,14 @@
+package com.andreibarroso.springionic.exceptions;
+
+public class FileException extends  RuntimeException {
+
+    public FileException(String msg) {
+        super(msg);
+
+    }
+
+    public FileException(String msg, Throwable cause) {
+        super(msg, cause);
+
+    }
+}
